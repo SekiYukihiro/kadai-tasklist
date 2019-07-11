f8cbd1d65e5d833bdbdea135cda3b203431beeae
hiroyuki.seki <shiba920sof@gmail.com>
2019-07-11 04:25:07 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Tasklists</h1>
+            {!! link_to_route('signup.get','Sign up',['class'=>'btn btn-primary btn-lg']) !!}
         </div>
     </div>
 @endsection
