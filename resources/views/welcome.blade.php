c901b0cc8345760a2397206c08f18a8b6b93f611
hiroyuki.seki <shiba920sof@gmail.com>
2019-07-11 22:29:20 +0000

    add user_id

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Tasklists</h1>
-            {!! link_to_route('signup.get','Sign up',['class'=>'btn btn-primary btn-lg']) !!}
+            {!! link_to_route('signup.get','Sign up now!',[],['class'=>'btn btn-primary btn-lg']) !!}
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,15 @@ Route::get('/',function(){
         return view('welcome');
 });
 
+// Route::get('/','TasksController@index');
+
 //ユーザ登録
 Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup','Auth\RegisterController@register')->name('signup.post');
 
-// Route::get('/','TasksController@index');
+//ログイン認証
+Route::get('login','Auth\LoginController@showloginForm')->name('login');
+Route::post('login','Auth\LoginController@login')->name('login.post');
+Route::get('logout','Auth\LoginController@logout')->name('logout.get');
 
 Route::resource('tasks','TasksController');
