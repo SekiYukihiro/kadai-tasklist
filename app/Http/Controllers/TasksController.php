f0289649589a5d93fb98c1db457cf3db442cd461
hiroyuki.seki <shiba920sof@gmail.com>
2019-07-13 20:39:41 +0000

    Controller&routes Update

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -15,11 +15,24 @@ class TasksController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
 
-        return view('tasks.index',[
-            'tasks' => $tasks,
-        ]);
+        $data=[];
+        if(\Auth::check()){
+            $user = \Auth::user();
+            $tasks = $user->tasks()->orderBy('created_at','desc')->paginate(10);
+
+            $data=[
+                'user' => $user,
+                'tasks' => $tasks,
+            ];
+
+            return view('tasks.index',$data);
+
+        }else{
+
+            return view('welcome',$data);
+
+        }
     }
 
     /**
@@ -49,11 +62,12 @@ class TasksController extends Controller
             'content' => 'required|max:191',
         ]);
 
-        $task = new Task;
-        $task->status = $request->status;
-        $task->content = $request->content;
-        $task->save();
+        $request->user()->tasks()->create([
+            'status' => $request->status,
+            'content' => $request->content,
+        ]);
 
+        // return back();
         return redirect('/');
     }
 
@@ -117,8 +131,11 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        $task = Task::find($id);
-        $task->delete();
+        $task = \App\Task::find($id);
+
+        if(\Auth::id() === $task->user_id){
+            $task->delete();
+        }
 
         return redirect('/');
     }
